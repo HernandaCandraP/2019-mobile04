ca638df0a1e06b21746243a7c7c6f41114516343
HernandaCandraP <hernandacp@gmail.com>
2019-09-18 16:41:05 +0700

    Menanmbahkan parcelable

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
@@ -3,8 +3,13 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import id.ac.polinema.intent.model.user;
 
 public class ProfileParcelableActivity extends AppCompatActivity {
+
+    private TextView usernameText, nameText, ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,11 +17,23 @@ public class ProfileParcelableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_parcelable);
 
         // TODO: bind here
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
 
             // TODO: display value here
+            user use = getIntent().getParcelableExtra("parcel");
+
+            String username = use.getUsername();
+            String name = use.getName();
+            int age = use.getAge();
+
+            usernameText.setText(username);
+            nameText.setText(name);
+            ageText.setText(String.valueOf(age));
         }
     }
 }
